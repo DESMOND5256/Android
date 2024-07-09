7d91530275365224937bf86c7469a85508dcca30
DESMOND5256 <113458344+DESMOND5256@users.noreply.github.com>
2024-07-09 12:48:19 -0700

    Add ChildFolder inside ParentFolder to repository

---
+++ Quiz_App/app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+public static String question[] = {
+
+    "Which company owns android",
+        "which one is not the programming language",
+           "Where are you watching this video?"
+
+};
+
+public static String choices[][] = {
+        {"Google","Apple","Nokia","Samsung"},
+           {"Java","Kotlin","NotePad","Python"},
+              {"Facebook","Whatsapp","Instagram","Youtube"},
+
+
+};
+
+public static String correctAnswers[] = {
+      "Google",
+        "NotePad",
+          "Youtube"
+
+};
+
+}
